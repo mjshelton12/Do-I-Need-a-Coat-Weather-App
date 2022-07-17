6739239b02dd66499b9e634e317c15d99a9921f9
MJ_Shelton <mjshelton12@comcast.net>
2022-07-16 19:11:19 -0700

    Rough framework of app

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+var requestUrl = "https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=28e15096387ef21bb429f687306a6643"
+var location = getElementById("#location")
+
+console.log(location)
+
+//Code must:
+//Pull city from location box
+//Place that city in the weather API search
+//Populate the site with its information
+//Put in a little weather icon in the forcast
+//Based on the weather, report if a coat is needed
